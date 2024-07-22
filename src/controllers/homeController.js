c8ef4b76da85a5887546cf4249b77e31c52b6bf0
nnhuong <nnhuong1763@gmail.com>
2024-07-22 23:25:56 +0700

    CRUDService homeController web

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -60,6 +60,18 @@ let putCRUD = async(req, res) => {
         dataTable: allUsers  
       });
 }
+let deleteCRUD = async(req, res)=>{
+    let id = req.query.id;
+    if(id){
+        await CRUDService.deleteUserById(id);
+        return res.send('Delete user succeed');
+    }
+    else {
+        return res.send('User not found');
+    }
+}
+
+
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
@@ -68,4 +80,5 @@ module.exports = {
     displayGetCRUD: displayGetCRUD,
     getEditCRUD: getEditCRUD,
     putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD,
 }
